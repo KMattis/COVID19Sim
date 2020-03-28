e5eeb4c4c2a93be22af184831dcb90c07b7cf5c1
Klaus Mattis <klaus.mattis@web.de>
2020-03-28 19:20:28 +0100

    fixed division by zero bug

--- model/person.py
+++ model/person.py
@@ -28,7 +28,8 @@ class Person:
         distance = math.sqrt((self.currentPosition.x - dest.x)**2 + (self.currentPosition.y - dest.y)**2)
         self.travelEnd.set(now.now() + distance * MINUTES_PER_CELL * random.uniform(0.9, 1.1) * time.MINUTE)
         self.currentDestination = dest
-        self.direction = [(self.currentDestination.x - self.currentPosition.x) / (self.travelEnd.now() - self.travelStart.now()), (self.currentDestination.y - self.currentPosition.y) / (self.travelEnd.now() - self.travelStart.now())]
+        divisor = max(1,(self.travelEnd.now() - self.travelStart.now()))
+        self.direction = [(self.currentDestination.x - self.currentPosition.x) / divisor, (self.currentDestination.y - self.currentPosition.y) / divisor]
     
     def getXY(self, now: time.Timestamp):
         if self.isTraveling():        
