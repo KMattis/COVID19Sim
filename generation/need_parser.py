45de408625e9c258a1438eb7df59be2299336ba3
Klaus Mattis <klaus.mattis@web.de>
2020-03-31 21:09:19 +0200

    organize imports in need_parser

--- generation/need_parser.py
+++ generation/need_parser.py
@@ -1,10 +1,5 @@
-import configparser
-from simulation import time
-from model import place, place_characteristics, needs
-
 import importlib.util
 import inspect
-
 
 def readNeedTypes(configFile):
     spec = importlib.util.spec_from_file_location("need_types", configFile)
