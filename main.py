e1dcb805aa0362235d06de1893119051755fd130
Klaus Mattis <klaus.mattis@web.de>
2020-04-01 20:21:03 +0200

    refactor main

--- main.py
+++ main.py
@@ -11,89 +11,99 @@ from simulation.simulation import Simulation
 from profiler.profiler import profilerObj
 from plotting import logging
 
-
-#Setup argparse
-argparser: argparse.ArgumentParser = argparse.ArgumentParser()
-argparser.add_argument("--no-render", dest="noRender", action='store_const', const=True)
-argparser.add_argument("--config-file", dest="configFile", type=str, default="simconfig/config.ini")
-argparser.add_argument("--need-types-file", dest="needTypesFile", type=str, default="simconfig/need_types.py")
-argparser.add_argument("--num-days", dest="numDays", type=int, default=0)
-args = argparser.parse_args()
-
 MINUTES_PER_REAL_SECOND = 100
+
+def readArguments():
+    #Setup argparse
+    argparser: argparse.ArgumentParser = argparse.ArgumentParser()
+    argparser.add_argument("--no-render", dest="noRender", action='store_const', const=True)
+    argparser.add_argument("--config-file", dest="configFile", type=str, default="simconfig/config.ini")
+    argparser.add_argument("--need-types-file", dest="needTypesFile", type=str, default="simconfig/need_types.py")
+    argparser.add_argument("--num-days", dest="numDays", type=int, default=0)
+    return argparser.parse_args()
 
 def getCurrentTimeMillis():
     return round(time.time() * 1000)
 
-logging.registerCategory("activity")
-logging.registerCategory("bobby")
-logging.registerCategory("output")
+def registerLoggingCategories():
+    logging.registerCategory("activity")
+    logging.registerCategory("bobby")
+    logging.registerCategory("output")
 
-config = configparser.ConfigParser()
-config.read(args.configFile)
+def loadPersonScripts(config):
+    personScriptName = config["default"]["personScript"]
+    spec = importlib.util.spec_from_file_location("person_script", personScriptName)
+    personScriptModule = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(personScriptModule)
+    members = dict((name, func) for name, func in inspect.getmembers(personScriptModule))
+    return members["initialize"], members["getNeedPrio"], members["updateNeeds"]
 
-logging.write("output", "generating")
-needTypes = generation.need_parser.readNeedTypes(args.needTypesFile)
-theGrid = generation.grid_generator.generate(int(config["default"]["gridSize"]), needTypes)
-persons = generation.person_generator.generate(theGrid, int(config["default"]["numPersons"]), needTypes)
+def main():
+    args = readArguments()
 
-for needType in needTypes:
-    needType.initialize(persons, theGrid)
-    logging.write("output", needType.getName())
+    registerLoggingCategories()
 
-personScriptName = config["default"]["personScript"]
-spec = importlib.util.spec_from_file_location("person_script", personScriptName)
-personScriptModule = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(personScriptModule)
-members = dict((name, func) for name, func in inspect.getmembers(personScriptModule))
-initializePersonScript = members["initialize"]
-getNeedPrio = members["getNeedPrio"]
-updateNeeds = members["updateNeeds"]
+    config = configparser.ConfigParser()
+    config.read(args.configFile)
 
-if not args.noRender:
-    theRenderer = Renderer(len(persons))
-    theRenderer.initPlaceBuffer(theGrid)
+    logging.write("output", "generating")
+    needTypes = generation.need_parser.readNeedTypes(args.needTypesFile)
+    theGrid = generation.grid_generator.generate(int(config["default"]["gridSize"]), needTypes)
+    persons = generation.person_generator.generate(theGrid, int(config["default"]["numPersons"]), needTypes)
 
-initializePersonScript(needTypes)
-theSimulation = Simulation(persons, getNeedPrio, updateNeeds)
+    for needType in needTypes:
+        needType.initialize(persons, theGrid)
+        logging.write("output", needType.getName())
 
-lastUpdate = getCurrentTimeMillis()
-running = True
-
-loops = 0
-
-logging.write("output", "starting main loop")
-while running:
-    profilerObj.startProfiling("covid")
-    now = getCurrentTimeMillis()
-    deltaTime = now - lastUpdate
+    initializePersonScript, getNeedPrio, updateNeeds = loadPersonScripts(config)
 
     if not args.noRender:
-        profilerObj.startProfiling("fetchEvents")
-        running = theRenderer.fetchEvents(deltaTime)
-        profilerObj.stopStartProfiling("render")
-        theRenderer.render(persons, deltaTime, theSimulation.now)
+        theRenderer = Renderer(len(persons))
+        theRenderer.initPlaceBuffer(theGrid)
+
+    initializePersonScript(needTypes)
+    theSimulation = Simulation(persons, getNeedPrio, updateNeeds)
+
+    lastUpdate = getCurrentTimeMillis()
+    running = True
+
+    loops = 0
+
+    logging.write("output", "starting main loop")
+    while running:
+        profilerObj.startProfiling("covid")
+        now = getCurrentTimeMillis()
+        deltaTime = now - lastUpdate
+
+        if not args.noRender:
+            profilerObj.startProfiling("fetchEvents")
+            running = theRenderer.fetchEvents(deltaTime)
+            profilerObj.stopStartProfiling("render")
+            theRenderer.render(persons, deltaTime, theSimulation.now)
+            profilerObj.stopProfiling()
+                
+        profilerObj.startProfiling("simulation")
+
+        for i in range(max(1, int(deltaTime / 1000 * MINUTES_PER_REAL_SECOND))):
+            theSimulation.simulate()
+            lastUpdate = getCurrentTimeMillis()
         profilerObj.stopProfiling()
-            
-    profilerObj.startProfiling("simulation")
 
-    for i in range(max(1, int(deltaTime / 1000 * MINUTES_PER_REAL_SECOND))):
-        theSimulation.simulate()
-        lastUpdate = getCurrentTimeMillis()
-    profilerObj.stopProfiling()
+        profilerObj.stopProfiling() #covid
+        loops += 1
+        if loops % 100 == 0:
+            now = theSimulation.now
+            print("Simulation time: {0}:{1}:{2}".format(now.day(), now.hourOfDay(), now.minuteOfHour()))
+            profilerObj.printPercentages("covid")
+            profilerObj.reset()
 
-    profilerObj.stopProfiling() #covid
-    loops += 1
-    if loops % 100 == 0:
-        now = theSimulation.now
-        print("Simulation time: {0}:{1}:{2}".format(now.day(), now.hourOfDay(), now.minuteOfHour()))
-        profilerObj.printPercentages("covid")
-        profilerObj.reset()
+        if args.numDays > 0 and theSimulation.now.day() >= args.numDays:
+            running = False
 
-    if args.numDays > 0 and theSimulation.now.day() >= args.numDays:
-        running = False
+    logging.write("output", "shutting down")
 
-logging.write("output", "shutting down")
+    if not args.noRender:
+        theRenderer.quit()
 
-if not args.noRender:
-    theRenderer.quit()
+if __name__ == "__main__":
+    main()
