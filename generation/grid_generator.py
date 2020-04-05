fbff4ad1bcf9f0135f509617c170f208bd354938
Klaus Mattis <klaus.mattis@web.de>
2020-04-05 12:39:36 +0200

    remove needTypes from places.ini; grid generation now relies only on the placChars frequency

--- generation/grid_generator.py
+++ generation/grid_generator.py
@@ -4,6 +4,7 @@ import random
 from model import grid, place, place_characteristics
 from simulation import time
 from generation import place_parser
+from plotting import logging
 
 def generate(model_data, needTypes):
     needTypesDict = {}
@@ -15,38 +16,28 @@ def generate(model_data, needTypes):
 
     theGrid = grid.Grid(size)
     
-    workplaceChars = place_parser.readPlacesWithPrefix(places_file, "work", needTypesDict)
-    workplaceCharFreqs = [char.frequency for char in workplaceChars]
-
-    outdoorChars = place_parser.readPlacesWithPrefix(places_file, 'outdoor', needTypesDict)
-    homeChars = place_parser.readPlacesWithPrefix(places_file, 'home', needTypesDict)
-    noneChar = place_characteristics.PlaceCharacteristics(place_characteristics.PlaceType.NONE,0,0,0,0, [None], place_characteristics.SubType.NONE, 0)
-    healthcareChars = place_parser.readPlacesWithPrefix(places_file, 'healthcare', needTypesDict)
+    chars = place_parser.readAllPlaceChars(places_file)
+    charFreqs = [char.frequency for char in chars]
 
     centerx = size / 2
     centery = size / 2
+
+    numPlaces = {}
+    for placeType in place_characteristics.PlaceType:
+        numPlaces[placeType] = 0
+
     for i in range(size):
         for j in range(size):
-            radius2 = (i - centerx)**2 + (j - centery)**2
-            if radius2 > size**2 / 4:
-                c = noneChar
-            else: 
-                maxRadius = size / 2
-                pWork = ((maxRadius - math.sqrt(radius2)) / maxRadius) * 0.6
-                pPark = 0.1
-                pHealthcare = 0.002
-                
-                p = random.random()
-
-                if p < pWork:
-                    c = random.choices(workplaceChars, workplaceCharFreqs)[0]
-                elif p < pWork + pPark:
-                    c = random.choice(outdoorChars)
-                elif p < pWork + pPark + pHealthcare:
-                    c = random.choice(healthcareChars)
-                else:
-                    c = random.choice(homeChars)
+            if (i - centerx)**2 + (j - centery)**2 > size**2 / 4:
+                c = place_characteristics.NONE_CHAR
+            else:
+                c = random.choices(chars, charFreqs)[0]
+            numPlaces[c.placeType] += 1
             theGrid.addPlace(place.Place(i, j, "", c))
+
+    for placeType in numPlaces:
+        logging.write("output", placeType.name, numPlaces[placeType])
+
     theGrid.getDistanceMap().calcDistances()
     return theGrid
 
