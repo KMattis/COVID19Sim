441cad78daa73734de2776276f0181c8dda90404
Julius Hermelink <hermelink.external@infineon.com>
2020-04-08 17:15:41 +0200

    Remove stations outside town

--- generation/mt_generator.py
+++ generation/mt_generator.py
@@ -1,4 +1,5 @@
 from model import mass_transportation as mt
+from model.place_characteristics import PlaceType
 
 
 def generate(grid, step, frequency):
@@ -6,9 +7,11 @@ def generate(grid, step, frequency):
     connections = []
     for x in range(0, grid.size, step):
         for y in range(0, grid.size, step):
-            stops.append(grid.get(x,y))
-            if x > 0:
-                connections.append((len(stops)-1, len(stops)-1 - grid.size//step, 1))
-            if y > 0:
-                connections.append((len(stops)-1, len(stops)-2, 1))
+            place = grid.get(x,y)
+            if place.char.placeType is not PlaceType.NONE:
+                stops.append(grid.get(x,y))
+                if x > 0:
+                    connections.append((len(stops)-1, len(stops)-1 - grid.size//step, 1))
+                if y > 0:
+                    connections.append((len(stops)-1, len(stops)-2, 1))
     return mt.TrafficNetwork(stops, connections, grid, frequency, 10) 
