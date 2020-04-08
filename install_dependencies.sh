3c32c459e14a91c9aebfdae4260b3508d4354f21
Klaus Mattis <klaus.mattis@web.de>
2020-04-08 16:09:07 +0200

    Split dependencies into render/noRender

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,4 @@
-pip3 install numpy
-pip3 install pygame
-pip3 install PyOpenGL
-pip3 install PyOpenGL_accelerate
-pip3 install matplotlib
+#!/bin/bash
+pip3 install -r install/dependencies_render.txt
+pip3 install -r install/dependencies.txt
 mkdir logfiles
